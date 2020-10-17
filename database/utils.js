cf90b02c687b9c30aad387f2a69c4c71e20d3bcb
Branden McCarthy <bmccarthy2@ucmerced.edu>
2020-10-17 13:22:54 -0700

    Corrected setUpdateString utility function

--- database/utils.js
+++ database/utils.js
@@ -3,6 +3,7 @@ const setUpdateString = (updateObject) => {
     let updates = "";
     for(let key of Object.keys(updateObject)) { updates += `${key} = '${updateObject[key]}', ` }
     updates = updates.substring(0, updates.length - 2); //remove last comma
+    return updates;
 }
 
 module.exports = { setUpdateString: setUpdateString }
