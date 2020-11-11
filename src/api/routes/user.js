5582146e4363c0f5d1a8ab8dc60e6f718835827f
Branden McCarthy <bmccarthy2@ucmerced.edu>
2020-11-11 14:57:49 -0800

    Added route to get user account details

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -4,6 +4,15 @@ const router = express.Router();
 const UserService = require('../../services/UserService');
 const authorizeJWT = require('../middlewares/authorization');
 
+router.get('/', authorizeJWT, async (req, res) => {
+    try {
+        const userRecord = await UserService.getAccountDetails(req.user.id);
+        return res.status(200).send({ success: true, message: "User retrieved", user: userRecord });
+    } catch(err) {
+        return res.status(400).send({ success: false, message: err.message });
+    }
+});
+
 router.delete('/', authorizeJWT, async (req, res) => {
     try {
         await UserService.deleteAccount(req.user.id);
@@ -16,7 +25,7 @@ router.delete('/', authorizeJWT, async (req, res) => {
 router.patch('/', authorizeJWT, async (req, res) => {
     try {
         const updatedUser = await UserService.changeAccountDetails(req.user.id, req.body);
-        return res.status(200).send({ message: "User update successful", user: updatedUser });
+        return res.status(200).send({ success: true, message: "User update successful", user: updatedUser });
     } catch(err) {
         return res.status(400).send({ success: false, message: err.message });
     }
@@ -25,10 +34,9 @@ router.patch('/', authorizeJWT, async (req, res) => {
 router.patch('/changePassword', authorizeJWT, async (req, res) => {
     try {
         await UserService.changePassword(req.user.id, req.body.password);
-        return res.status(200).send({ message: "Password update successful", success: true });
+        return res.status(200).send({ success: true, message: "Password update successful" });
     } catch(err) {
-        console.log(err);
-        return res.status(400).send({ success: false, message: "Password not updated", error: err });
+        return res.status(400).send({ success: false, message: "Password not updated" });
     }
 });
 
